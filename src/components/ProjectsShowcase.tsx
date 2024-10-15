f4bfaa1ab84679838f69cbf9c9d02055639f589e
Eugene <eugeneskom@gmail.com>
2024-10-16 01:50:16 +0300

    cleanded app ProjectsShowcase

--- src/components/ProjectsShowcase.tsx
+++ src/components/ProjectsShowcase.tsx
@@ -7,88 +7,7 @@ import { ExternalLink, Github } from 'lucide-react';
 import Image from 'next/image';
 import axios from 'axios';
 
-const projects = [
-  // {
-  //   title: "Full-Stack Logistics Management Platform",
-  //   description: "A comprehensive logistics management system for trucking companies.",
-  //   technologies: ["React", "Node.js", "MariaDB", "Express", "TypeScript", "Tailwind.css", "Rest API"],
-  //   liveLink: "https://remberg.eugeneskom.com/",
-  //   githubLink: "https://github.com/eugeneskom/remberg-front",
-  //   image: "/images/truckboard.png"
-  // },
-  // {
-  //   title: "E-commerce Website Dream Moments",
-  //   description: "A full-featured online store with product management and order processing.",
-  //   technologies: ["SCSS", "HTML", "TypeScript"],
-  //   liveLink: "https://dream-moments.kz/",
-  //   githubLink: "https://github.com/eugeneskom/dream-moments",
-  //   image: "/images/dream-moments.png"
-  // },
-  // {
-  //   title: "Fonte Capital Ltd",
-  //   description: "A Chrome extension integrating WhatsApp chat functionality for CRM systems.",
-  //   technologies: ["HTML", "SCSS", "JavaScript", "REST API"],
-  //   liveLink: "https://fonte.kz/",
-  //   githubLink: "https://github.com/eugeneskom/fontee",
-  //   image: "/images/fontee.png"
-  // },
-  // {
-  //   title: "Interactive Budget Allocation Simulator",
-  //   description: "An interactive web application simulating large-scale spending, featuring dynamic updates and real-time calculations.",
-  //   technologies: ["TypeScript", "React", "WordPress", "REST API"],
-  //   liveLink: "https://bedsteitest.dk/brug-sanjay-shahs-svindelformue/",
-  //   githubLink: "",
-  //   image: "/images/calculator.png"
-  // },
-  // {
-  //   title: "ICVR - Cutting Edge Software, Art, and Games",
-  //   description: "An award-winning digital production company specializing in immersive experiences, virtual production, and game development",
-  //   technologies: ["SCSS", "HTML", "JavaScript", "WordPress"],
-  //   liveLink: "https://icvr.io/",
-  //   githubLink: "",
-  //   image: "/images/icvr.png"
-  // },
-  // {
-  //   title: "TATI SIMPSON PHOTOGRAPHY",
-  //   description: "Designed and developed a striking, emotion-driven portfolio website for a globe-trotting wedding photographer.",
-  //   technologies: ["SCSS", "HTML", "JavaScript", "WordPress"],
-  //   liveLink: "https://tatisimpson.com/",
-  //   githubLink: "",
-  //   image: "/images/tati.png"
-  // },
-  // {
-  //   title: "Medical Appointment Scheduler for Microsoft 365",
-  //   description: "Developed a comprehensive medical appointment scheduling system integrated with Microsoft 365, using React.js. This application streamlines the process of booking and managing medical appointments for healthcare providers.",
-  //   technologies: ["SCSS", "HTML", "JavaScript", "WordPress"],
-  //   liveLink: "",
-  //   githubLink: "",
-  //   image: "/images/medical-todo.png"
-  // },
-  // {
-  //   title: "CRM Chrome Extension",
-  //   description: "A Chrome extension integrating WhatsApp chat functionality for CRM systems.",
-  //   technologies: ["TypeScript", "Chrome API", "React", "Firebase", "REST API"],
-  //   liveLink: "https://chromewebstore.google.com/detail/funner/jkhecbecigibhenciiplohmdnkclhpfk",
-  //   githubLink: "",
-  //   image: "/images/funner.png"
-  // },
-  // {
-  //   title: "Analytics Dashboard",
-  //   description: "An interactive dashboard for visualizing e-commerce analytics and KPIs.",
-  //   technologies: ["React", "D3.js", "Node.js", "MongoDB"],
-  //   liveLink: "https://dashboard.example.com",
-  //   githubLink: "https://github.com/yourusername/analytics-dashboard",
-  //   image: "/api/placeholder/800/600"
-  // },
-  // {
-  //   title: "Site Builder",
-  //   description: "An interactive dashboard for visualizing e-commerce analytics and KPIs.",
-  //   technologies: ["React", "D3.js", "Node.js", "MongoDB"],
-  //   liveLink: "https://dashboard.example.com",
-  //   githubLink: "https://github.com/yourusername/analytics-dashboard",
-  //   image: "/api/placeholder/800/600"
-  // }
-];
+ 
 
 interface Project {
   title: string;
@@ -139,6 +58,7 @@ const ProjectsShowcase: React.FC = () => {
     const fetchProjects = async () => {
       try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/wp-json/wp/v2/project?_embed`);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const fetchedProjects = response.data.map((project: any) => ({
           title: project.title.rendered,
           description: project.project_meta.description || '',
