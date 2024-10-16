a831920a3b5a0586845a75c84fd57b1e5eeaa8ed
Eugene <eugeneskom@gmail.com>
2024-10-16 04:14:38 +0300

    removed AvatarImage

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback,
+  //  AvatarImage
+   } from '@/components/ui/avatar';
 import { Quote } from 'lucide-react';
 
 const testimonials = [
