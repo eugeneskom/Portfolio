5cb77e3d18f8d6e7937e27edcb58c537529dde05
Eugene <eugeneskom@gmail.com>
2024-10-19 10:22:34 +0300

    removed cart from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,7 @@ import Testimonials from "@/components/Testimonials";
 import BlogArticles from "@/components/BlogArticles";
 import ContactForm from "@/components/ContactForm";
 
-import { sql } from "@vercel/postgres";
 
-export async function Cart({
-  params
-} : {
-  params: { user: string }
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
-
-  return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
-  );
-}
 
 export default function Home() {
   return (
